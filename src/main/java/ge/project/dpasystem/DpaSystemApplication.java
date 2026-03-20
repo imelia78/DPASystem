f383e1f4a953998f193fb71c685906789554d14b
Ilia Melia <ilyamelka2004@gmail.com>
2026-03-20 19:55:54 +0400

    initial config

---
+++ src/main/java/ge/project/dpasystem/DpaSystemApplication.java
@@ -0,0 +1,13 @@
+package ge.project.dpasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/project/dpasystem/DpaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.project.dpasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
