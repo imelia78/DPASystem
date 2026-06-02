0805c7c920fc182ea3afb974879d5876478939b7
Илья <ilia@MacBook-Pro-Ila.local>
2026-06-02 14:37:16 +0400

    KeyCloak auth implemented

---
+++ src/main/java/ge/project/dpasystem/config/KeycloakJwtAuthenticationConverter.java
@@ -0,0 +1,28 @@
+package ge.project.dpasystem.config;
+
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        List<String> roles = source.getClaimAsStringList("roles");
+        if (roles == null) {
+            return Collections.emptyList();
+        }
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/ge/project/dpasystem/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package ge.project.dpasystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/actuator/**").hasAuthority("ROLE_dpasystem.admin")
+                                .requestMatchers("/api/v1/clients/**").hasAnyAuthority("ROLE_dpasystem.admin","ROLE_dpasystem.user")
+                                .requestMatchers("/api/v1/appointments/**").hasAuthority("ROLE_dpasystem.admin")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
+        return http.build();
+    }
+
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter( new KeycloakJwtAuthenticationConverter());
+        return converter;
+    }
+
+}
